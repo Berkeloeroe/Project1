16125bfd45faa0a568d8357bd514eb3d4a98c034
Your Name <2108388@talnet.nl>
2020-09-07 21:09:58 +0200

    Log in

---
+++ index.php
@@ -0,0 +1,16 @@
+<div class= "log in-form">
+
+<form>
+
+ <input type="text" name="username" placeholder="Username"><br>
+ <input type="password" name="password" placeholder="Password"><br>
+
+ <button type="submit" class="button">Log in</button><br>
+
+<a class="vergeten" href="http://localhost/project1/signup.php">Make new account</a>
+<br>
+<a class="vergeten" href="http://localhost/project1/lostpsw.php">Forgot Password</a>
+
+</form>
+
+</div>
