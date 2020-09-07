32f7b8b554d7dfdbb095fd08930d3f2bcb764463
Your Name <2108388@talnet.nl>
2020-09-07 21:14:17 +0200

    Reset password

---
+++ lostpsw.php
@@ -0,0 +1,12 @@
+<div class="Forgot password-form">
+
+  <form>
+
+    <input type="text" name="Email" placeholder="Username"/>
+    <br>
+    <button type="submit" class="button">Confirm</button>
+    <br>
+
+  </form>
+
+</div>
