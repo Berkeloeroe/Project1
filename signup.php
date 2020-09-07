f724a70e5fac6bec01c2c6295244437ae7d44da0
Your Name <2108388@talnet.nl>
2020-09-07 21:12:42 +0200

    Sign up

---
+++ signup.php
@@ -0,0 +1,19 @@
+<div class= "Make-form">
+
+<form>
+
+ <input type="text" name="Voornaam" placeholder="First Name"><br>
+ <input type="text" name="Tussenvoegsel" placeholder="Insertion"><br>
+ <input type="text" name="Achternaam" placeholder="Last Name"><br>
+ <input type="text" name="E-mail" placeholder="E-mail"><br>
+ <input type="text" name="gebruikersnaam" placeholder="Username"><br>
+ <input type="text" name="wachtwoord" placeholder="Password"><br>
+ <input type="text" name="herhaal_wachtwoord" placeholder="Repeat Password"><br>
+
+ <button type="submit" class="button">Make an account</button>
+
+ <br>
+
+</form>
+
+</div>
