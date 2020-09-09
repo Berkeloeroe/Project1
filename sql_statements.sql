1af9b1a8e272748218d8d79e5e0102d24feff45a
Your Name <2108388@talnet.nl>
2020-09-09 15:24:26 +0200

    foreign key erbij en verbeteringen die hopelijk goed zijn

--- sql_statements.sql
+++ sql_statements.sql
@@ -8,10 +8,12 @@ CREATE TABLE account (
 CREATE TABLE persoon (
     ID int NOT NULL AUTO_INCREMENT,
     firstName varchar(250),
-    tussenVoegsel varchar(250),
-    achterNaam varchar(250),
+    insertion varchar(250),
+    lastName varchar(250),
     userName varchar(250),
-    PRIMARY KEY (ID)
+    account_ID int NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (account_ID) REFERENCES account(ID)
 );
 
     
